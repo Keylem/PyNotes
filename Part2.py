807b2fba0188f50eb3e9692756a30c4d6be4e115
Keylem <52345143+Keylem@users.noreply.github.com>
2020-12-17 22:09:33 +0300

    Add files via upload

---
+++ Part2.py
@@ -0,0 +1,70 @@
+#Bir işlemi tek satırda yapma
+#Stringlerde işlemler
+#Input
+
+###############- Tekrar -#################
+"""
+ 
+Primitive:
+    Temel yapı taşları:
+        integer ----> int() tamsayı
+        float   ----> float() kesirli sayı
+
+    Bilgisayarın Anlayabildiği işler
+    Hesaplamalar
+    Karakterler:
+        Bilgisayarın sonradan öğrendiği işler
+        Arayüz, klavye
+        Alfabe, Emoji UTF-8
+
+Reference:
+    Birden fazla temel yapı taşlarının birlikte kullanılmasına reference tipleri diyoruz:
+        Başlangıç ve bitiş değerleri vardır
+        Herhangi bir matematiksel değer taşımazlar
+
+        bu tipler:
+            Stringler ( iplik )  Alfabedeki karakterlerin oluşturduğu özel listelerdir. 
+            #Listeler orataya karışık
+"""
+"""
+Operatörler:
+    Atama operatörü "="
+    Toplama operatörü "+"
+    Çıkarma operatörü "-"
+    Çarpma operatörü "*"
+    Bölme operatörü "/"
+    Tam bölme operatörü "//"
+    Üssünü alma operatörü "**"
+    *new* bölümünden kalan operatörü "%"
+"""
+
+"""
+Aynı türler iç içe geçebilir!
+        "Aşırı yükleme"
+"""
+
+x = "Kerem "
+y = "Çok"
+f = "Yakışıklı"
+
+z = x + y + f
+
+#Kerem Çok Yakışıklı
+
+
+print(list(z))
+
+"""
+Liste düzenleme
+[başlangıç : bitiş : kaçar kaçar]
+"""
+deneme1 = "0123456789"
+"asd"
+print(deneme1[0:9:2])
+print(max(deneme1))
+
+print("")
+
+"""
+Artist type strings
+"""
